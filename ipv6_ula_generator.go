5ad992667d46a8196aa7e521ded28d87490f400d
grusha4669 <grusha4669@gmail.com>
2026-09-10 12:32:57 +0300

    20260910-123257

--- ipv6_ula_generator.go
+++ ipv6_ula_generator.go
@@ -1,9 +1,3 @@
-// Copyright (C) 2026 grusha4669
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License.
-
 package main
 
 import (
